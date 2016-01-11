b9040178cbe6b5f5ac0965b55e1dc9a4fd33c40c
Artem Apartsev <phoenixtim@yandex.ru>
2016-01-11 23:42:26 +0600

    Admin site opened.

--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^', include('my_task_manager.urls', namespace='my_task_manager')),
     url(r'^authorization/', include('authorization.urls',
                                      namespace='authorization')),
